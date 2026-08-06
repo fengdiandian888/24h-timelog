f2f6fd83a60607305fe1cd8dbd7aba721a3d76ee
fengdiandian888 <84872469@qq.com>
2026-08-06 16:23:36 +0800

    PWA sync: index.html + manifest + sw.js + icons (6 files)

---
+++ sw.js
@@ -0,0 +1,22 @@
+/* 时间记录台 SW：网络优先 + 离线兜底 cache */
+const C = 't24-v1';
+const ASSETS = ['./', './index.html', './manifest.webmanifest', './icon-192.png', './icon-512.png'];
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(C).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
+});
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(ks => Promise.all(ks.filter(k => k !== C).map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
+});
+self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    fetch(e.request).then(r => {
+      const cp = r.clone();
+      caches.open(C).then(c => c.put(e.request, cp)).catch(() => {});
+      return r;
+    }).catch(() => caches.match(e.request).then(m => m || caches.match('./index.html')))
+  );
+});
